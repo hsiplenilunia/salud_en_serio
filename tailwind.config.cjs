db336ed1693b263f44538bfafc2e21d6c41d0d4a
ByteRogue <mystogan785@gmail.com>
2026-01-21 17:19:44 -0600

    Creación de la sección notas y sus card con diseño molecular

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,28 +8,18 @@ module.exports = {
 		,
 		extend: {
 			colors: {
-				whitegray: "#F2F2F2",
-				purple: "#7758AC",
-				'purple-text': "#7244B4",
-				apple: "#CCF750",
-				guacamole: "#C2D293",
-				'guacamole-light': "#D9E4BB",
-				'apple-dark':"#B7D280",
-				'pink-fresh':"#F8C5FE",
-				'black-text': "#231F20",
-				capuchino: "#EBE5DD",
-				'light-text': "#FAF9F9",
-
-				'navy': "#144157",
-				'light-gray':"#f2f2f2",
-				'gray-hsi': "#808080",
+				'navy': "#3422A0",
+				'light-gray':"#F2F2F2",
+				'black-text': "#000000",
 				'bg-gray': "#e9e9e9",
-				'hsi-blue': "#00ccf2",
-				'celeste-hsi': "#00bbe7",
-				'green-hsi': "#00B851",
+				'gray-button': "#D6D5D5",
+				'gray-card': "#D9D9D9",
+				'back-gray': "#BABABA",
 				'gradient-start': "#334757",
 				'gradient-center': "#00b0e3",
 				'gradient-end': "#62ac58",
+
+				
 			}
 		},
 	},
