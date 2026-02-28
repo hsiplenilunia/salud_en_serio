5a13830a5b58cc9b4722866cbda102cd2d0dd91c
Leonardo Ernesto Correa Hernández <aztharot.ru@proton.me>
2026-02-27 19:29:53 -0600

    Add MDX integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   output: 'static'
 });
